dcc06acdb057228024027a8475694e0e695219e8
Mike <mike@Mikes-MacBook-Air.local>
2025-10-18 20:09:12 -0400

    Switch from bcrypt to bcryptjs to fix Render deploy error

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { initDB } from "./db.js";
